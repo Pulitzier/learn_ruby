7c1b847b92cfb803692f390cfa25ebc81c17553d
ybaravy <ybrv2010@gmail.com>
2017-06-27 23:49:19 +0300

    Calculator solved and minor changing of README

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -12,3 +12,21 @@ def sum(ar)
 	ar.each { |i| s+=i };
 	return s;
 end
+
+def multiply(arr)
+	mul = 1;
+	arr.each { |i| mul*=i };
+	return mul;
+end
+
+def power(g,h)
+	g ** h
+end
+
+def factorial(n)
+	if n <= 1
+		1
+	else 
+		n*factorial(n-1)
+	end
+end	
