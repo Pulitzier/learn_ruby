0bb3585e367fc0d0a2d302c3ab999f45093db21e
ybaravy <ybrv2010@gmail.com>
2017-07-03 11:25:43 +0300

    Strange language done

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,40 @@
 #write your code here
+def helper(str)
+	case str.slice(0)
+	when /[aeiou]/
+		str + "ay"
+	when /[^aeiou]/
+		if /[^aeiou]/ === str[1]
+			if /[^aeiou]/ === str[2]
+				str.slice(3, str.length) + str[0] + str[1] + str[2] + "ay"
+			elsif str.index("qu")
+			str.slice(str.index("qu")+2, str.length) + str.slice(0, str.index("qu")+2) + "ay"
+			else
+			str.slice(2, str.length) + str[0] + str[1] + "ay"
+			end
+		elsif str.index("qu")
+		str.slice(2, str.length) + str.slice(0, 2) + "ay"	
+		else
+		str.slice(1, str.length) + str[0] + "ay"
+		end
+	else
+		puts "Doesn`t match to any regexp"
+	end
+	# puts "helper #{str}"
+end
+
+def translate(w)
+	if w.index(" ")
+		arr = w.split(" ")
+		i = 0
+		begin
+			arr[i] = helper(arr[i]) 
+			i += 1
+		end until i >= arr.length
+		w = arr.join(" ")
+
+		
+	else
+		helper(w)
+	end 
+end
