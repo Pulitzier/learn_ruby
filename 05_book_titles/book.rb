4c203be52b4f702ef9f5909791eb7243bf12ba21
ybaravy <ybrv2010@gmail.com>
2017-07-05 09:47:24 +0300

    Solution for 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+attr_reader :title
+
+def title=(t)
+	new_t = t.split(" ")
+	new_t = [new_t[0].capitalize] + 
+		new_t[1..-1].map do |w|
+			each = %w{and in the of a an}
+			if each.include? w
+				w
+			else
+				w.capitalize
+			end
+		end
+	@title = new_t.join(" ")
+end
 end
